b6e4af10a0a52f4d6ef72b0423ed24e22af69884
Andrey Sukhanov <sukhanov.licey@gmail.com>
2024-08-19 16:20:01 +0300

    Проверка

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import { objectWrapper } from './common'
+import { objectWrapper } from './common';
 
 function rootFunction() {
-  const myObj = { a: 1, b: '1', c: true, d: {k: 1234, l: 5678} }
+  const myObj = { a: 1, b: '1', c: true, d: { k: 1234, l: 5678 } };
+  const cloneMutableObj = objectWrapper(myObj, { a: 5, c: false });
 
-  const cloneMutableObj = objectWrapper(myObj, { a: 5, c: false })
-
-  console.log('original object:', myObj)
-  console.log('clone mutable object:', cloneMutableObj)
+  console.log('original object:', myObj);
+  console.log('clone mutable object:', cloneMutableObj);
 }
 
-rootFunction()
+rootFunction();
